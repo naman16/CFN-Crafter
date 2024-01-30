b0dab2968c0a38e0b9f1f735c61c733c3fcb5f3e
Naman <namansogani@gmail.com>
2024-01-30 21:35:25 +0000

    Adding formatted code

--- index.py
+++ index.py
@@ -10,12 +10,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_PROMPT
 from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
 from langchain.prompts import PromptTemplate
-from langchain.prompts import (
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-    ChatPromptTemplate,
-    MessagesPlaceholder
-)
+from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate,MessagesPlaceholder
 from streamlit_chat import message
 from langchain.schema import AIMessage, HumanMessage, format_document
 import streamlit as st
@@ -24,47 +19,62 @@ import random
 import time
 
 # provide OpenAI API key
-openai_key = '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'
-
+openai_key = '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'
 # Setting up OpenAI LLM model
 llm = ChatOpenAI(openai_api_key=openai_key, model_name="gpt-4-turbo-preview")
-
 embeddings = OpenAIEmbeddings(openai_api_key=openai_key)
 
-# Loading knowledge base
-loader = DirectoryLoader("data/", glob="output1.txt")
+system_message_template = SystemMessagePromptTemplate.from_template(template="""
+You are a cloud engineer that is an expert in Terraform for AWS and you are able to write full blown, production ready Terraform modules based on user requirements. 
+If the answer is not in the context say "Sorry, I don't know, as the answer was not found in the context."
+""")
 
+human_message_template = HumanMessagePromptTemplate.from_template(template="{input}")
 
-documents = loader.load()
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 1000,
-    chunk_overlap  = 200,
-)
+# Method to load docs / knowledge base and split into smaller chunks
+def load_and_split_docs():
+    loader = DirectoryLoader("data/", glob="output1.txt")
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap  = 200,
+    )
+    docs = text_splitter.split_documents(documents)
+    return docs
 
-docs = text_splitter.split_documents(documents)
+# Method to store chunks into a vectore store (FAISS)
+def creat_and_store_vector_embeddings():
+    vectorstore_faiss = FAISS.from_documents(
+        docs,
+        embeddings,
+    )
+    vectorstore_faiss.save_local("local_index")
+    return vectorstore_faiss
 
-vectorstore_faiss = FAISS.from_documents(
-    docs,
-    embeddings,
-)
+# Condense the chat history and follow-up question into a standalone question
+def conversational_retrieval_prompts_template():
+    _template = """{chat_history}
 
-vectorstore_faiss.save_local("local index")
+Answer only with the new question.
+How would you ask the question considering the previous conversation: {question}
+Question:"""
+    CONVO_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+    return CONVO_QUESTION_PROMPT
 
-
-
-
-
-
-
-
-retriever = vectorstore_faiss.as_retriever()
-
-wrapper_store_faiss = VectorStoreIndexWrapper(vectorstore=vectorstore_faiss)
-print(wrapper_store_faiss.query("Terraform for AWS ACM", llm=llm))
-
-memory = ConversationBufferMemory()
-memory.chat_memory.add_user_message("You are a cloud engineer that is an expert in writing Terraform code for AWS based on the information included in the data / context provided by me")
-memory.chat_memory.add_ai_message("I am career coach and give career advice")
-conversation = ConversationChain(
-     llm=llm, verbose=True, memory=memory
-)
+if __name__ == "__main__":
+    docs = load_and_split_docs()
+    vectorstore_faiss = creat_and_store_vector_embeddings()
+    wrapper_store_faiss = VectorStoreIndexWrapper(vectorstore=vectorstore_faiss)
+    memory_chain = ConversationBufferMemory(memory_key="chat_history", input_key="question", return_messages=True)
+    chat_history=[]
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore_faiss.as_retriever(),
+        memory=memory_chain,
+        condense_question_prompt=conversational_retrieval_prompts_template(),
+        chain_type='map_reduce'
+    )
+    prompt_template=ChatPromptTemplate.from_messages([system_message_template, MessagesPlaceholder(variable_name="history"), human_message_template])
+    qa.combine_docs_chain.llm_chain.prompt = PromptTemplate.from_template(prompt_template)
+    qa.predict(input="How to fix my car?")
+    
--- test.py
+++ test.py
@@ -1,31 +1,21 @@
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-    
-retrieved_docs = format_docs(retrieved_docs)
-print (retrieved_docs)
-    
-def run_streamlit_app():
-    st.title("Echo Bot")
+def create_prompt_template():
+    _template = """{chat_history}
 
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-    
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-    
-    # React to user input
-    if prompt := st.chat_input("What is up?"):
-        # Display user message in chat message container
-        st.chat_message("user").markdown(prompt)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-    
-        response = f"Echo: {prompt}"
-        # Display assistant response in chat message container
-        with st.chat_message("assistant"):
-            st.markdown(response)
-        # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
+Answer only with the new question.
+How would you ask the question considering the previous conversation: {question}
+Question:"""
+    CONVO_QUESTION_PROMPT = PromptTemplate.from_template(_template)
+    return CONVO_QUESTION_PROMPT
+
+memory_chain = ConversationBufferMemory(memory_key="chat_history", input_key="question", return_messages=True)
+chat_history=[]
+
+qa = ConversationalRetrievalChain.from_llm(
+    llm=llm, 
+    retriever=vectorstore_faiss.as_retriever(search_type='similarity', search_kwargs={"k": 8}),
+    memory=memory_chain,
+    #verbose=True,
+    condense_question_prompt=create_prompt_template(), 
+    chain_type='stuff', # 'refine',
+    #max_tokens_limit=100
+)
